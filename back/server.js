77ef7b394d7374ea3a53e035506930e70eb759e1
Quence1151 <dlgksdyd1151@naver.com>
2022-04-14 20:49:10 +0900

    feat: 4/14 sequelize 모델 생성 완료

--- back/server.js
+++ back/server.js
@@ -1,39 +1,8 @@
 const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
-const { sequelize } = require('./models');
+const user_inform = require('./routes/user_inform');
 
-const mainRouter = require('./routes/main.js');
-const postRouter = require('./routes/post.js');
-const memberRouter = require('./routes/member.js');
-const adminRouter = require('./routes/admin.js');
-const replyRouter = require('./routes/reply.js');
-const inquiryRouter = require('./routes/inquiry.js');
+app.use('/user_inform', user_inform);
 
-
-sequelize.sync({ force: false })
-    .then(() => {
-        console.log('데이터베이스 연결 성공')
-    })
-    .catch((err) => {
-        console.error(err);
-    })
-
-app.use(memberRouter);
-app.use(mainRouter);
-app.use(postRouter);
-app.use(adminRouter);
-app.use(replyRouter);
-app.use(inquiryRouter);
-
-app.use((req, res, next) => {
-    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-    error.status = 404;
-    next(error);
-});
-
-
-http.listen(3001, () => {
-    console.log('3001번포트로 실행중');
-});
-
+const port = 3001;
+app.listen(port, () => console.log(`Node.js Server is running on port ${port}...`));
