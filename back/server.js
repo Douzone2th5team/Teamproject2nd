5a7fd4b723f13458d7abe2c1b54b2d26a40295bd
Quence1151 <dlgksdyd1151@naver.com>
2022-04-15 19:58:49 +0900

    feat: 4/14 sequelize 모델 생성 완료

--- back/server.js
+++ back/server.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
-const user_inform = require('./routes/user_inform');
+const { sequelize } = require('./models');
 
-app.use('/user_inform', user_inform);
+sequelize.sync({ force: false })
+    .then(() => {
+        console.log('데이터베이스 연결 성공')
+    })
+    .catch((err) => {
+        console.error(err);
+    })
 
-const port = 3001;
-app.listen(port, () => console.log(`Node.js Server is running on port ${port}...`));
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+});
+http.listen(3001, () => {
+    console.log('3001번포트로 실행중');
+});
+
